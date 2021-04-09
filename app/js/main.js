abc482438cb134373e300a6985d9bc863e3441ba
fisherm4n <4143322@inbox.ru>
2021-04-09 05:49:00 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $(".about__accordion-tab").click(function () {
+    $(".about__accordion-tab").each(function () {
+      $(this).removeClass("about__accordion-tab--active");
+    });
+    $(this).addClass("about__accordion-tab--active");
+  });
+});
